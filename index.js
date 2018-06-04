da236060eaa3e7b01321980598e77688d6710022
Noah Salaman <nsalaman@iu.edu>
2018-06-04 17:39:32 -0400

    testing

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('testing')
+const button = document.querySelector('button')
+
+const update1 = function() {
+    const heading = document.querySelector('h1')
+    heading.textContent = 'Spellbook 2'
+
+}
+
+
+const button = document.querySelector('button')
+
+const sayContents = function() {
+  const heading = document.querySelector('h1')
+  heading.textContent = 'I <3 JavaScript'
+}
+
+button.addEventListener('click', sayContents)
+
+sayContents()
