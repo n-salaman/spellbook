7da411345eb65650a7145ecd47529e836fb8f9a2
Noah Salaman <nsalaman@iu.edu>
2018-06-05 10:08:18 -0400

    got final extra bonus thingy working

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const button = document.querySelector('button')
+document.getElementById("frm").addEventListener('submit', changeHead);
 
 function changeHead() {
   const x = document.getElementById("frm")
@@ -8,7 +8,24 @@ function changeHead() {
 
 }
 
-button.addEventListener('click', changeHead)
+const form = document.querySelector('form')
+
+form.addEventListener('submit')
+form.addEvne
+
+
+
+/*var input = document.getElementById("newInput")
+input.addEventListener("keyup", function(event) {
+    event.preventDefault()
+    if (event.keyCode === 13) {
+        alert("hello")
+        document.getElementById("butt").click()
+    }
+})
+
+<button id="butt" onclick="changeHead()">Submit</button>
+*/
 
 
 
