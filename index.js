5cf2671a3fcf7764c94ae6a71b640ab9943ccb5a
Noah Salaman <nsalaman@iu.edu>
2018-06-06 10:00:41 -0400

    created two different fields with different styles

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const form = document.querySelector('form')
 
+/*const spellSpan = function(spellName) {
+    const spellSpn = document.createElement("SPAN")
+    spellSpn.textContent = spellName
+    spellSpn.setAttribute('class', 'spell')
+    document.getElementById("spells").appendChild(spellSpn)
+}*/
+
 const changeHeading = function(ev) {
   ev.preventDefault()
 
@@ -7,22 +14,28 @@ const changeHeading = function(ev) {
   const spellName = f.spellName.value
   const castSpeed = f.castSpeed.value
 
-  const spellsDiv = document.querySelector('#spells')
+  //const spellsDiv = document.querySelector('#spells')
 
-  const div = document.createElement("div")
-  div.textContent=spellName
-  document.getElementById("spells").appendChild(div)
+  const spellSpn = document.createElement("SPAN")
+  spellSpn.textContent = spellName
+  spellSpn.setAttribute('class', 'spell')
+  document.getElementById("spells").appendChild(spellSpn)
+
+  document.getElementById("spells").appendChild(document.createElement("br"))
+
+  const castSpn = document.createElement("SPAN")
+  castSpn.textContent = `-${castSpeed} magic seconds`
+  castSpn.setAttribute('class', 'cast')
+  document.getElementById("spells").appendChild(castSpn)
   
-  const div2 = document.createElement("div")
+  /*const div2 = document.createElement("div")
   div2.textContent=`-${castSpeed} magic seconds`
-  document.getElementById("spells").appendChild(div2)
+  document.getElementById("spells").appendChild(div2)*/
 
   f.reset()
 }
 
-/*const spellSpan = function(spellName) {
-    const spellS
-}*/
+
 
 form.addEventListener('submit', changeHeading)
 
